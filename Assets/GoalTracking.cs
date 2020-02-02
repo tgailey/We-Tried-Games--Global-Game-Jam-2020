2abea4db9532541a4c3a656171461811ce1b8c6b
Trenton Gailey <trenton.gailey@gmail.com>
2020-02-01 19:03:54 -0700

    updated scene for LWRP

---
+++ Assets/GoalTracking.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTracking : MonoBehaviour
+{
+    public GameObject Goal;
+    private GameObject goalIcon;
+    public Material TransparentMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalIcon = new GameObject();
+        List<Material> materials = new List<Material>();
+        for(int i = 0; i < Goal.GetComponent<Renderer>().materials.Length; i++)
+        {
+            Material oldMat = Goal.GetComponent<Renderer>().materials[i];
+
+            Material newMat = new Material(TransparentMaterial);
+            newMat.color = new Color(oldMat.color.r, oldMat.color.g, oldMat.color.b, newMat.color.a);
+
+            materials.Add(newMat);
+        }
+
+        goalIcon.AddComponent<MeshFilter>().sharedMesh = Goal.GetComponent<MeshFilter>().sharedMesh;
+        goalIcon.AddComponent<MeshRenderer>().sharedMaterials = materials.ToArray();
+
+        goalIcon.transform.position = Goal.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
