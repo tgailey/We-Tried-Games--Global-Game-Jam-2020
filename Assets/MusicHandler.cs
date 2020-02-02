32a3713cb6917e5538f0e4b66ae83053993c6bde
Trenton Gailey <trenton.gailey@gmail.com>
2020-02-02 15:45:55 -0700

    updates

---
+++ Assets/MusicHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] sources = GetComponentsInChildren<AudioSource>();
+        sources[Random.Range(0, sources.Length)].enabled = (true);
+    }
+}
